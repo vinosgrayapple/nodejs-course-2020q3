cefbf1478b05837227e017fc3b30922efa588c6b
Serhii Komarychev <komarichev@gmail.com>
2020-10-29 20:35:20 +0200

    remove comments

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
-const { logger } = require('./lib/mlog')
-process.on('SIGINT', () => {
-  process.kill(process.pid)
-})
-logger.info(`PID: ${process.pid}`)
-const { connectDB } = require('./db')
 const { PORT } = require('./common/config')
+const { connectDB } = require('./db')
 const app = require('./app')
 connectDB(() => {
   app.listen(PORT, () => {
     logger.info(`App is running on http://localhost:${PORT}`)
   })
 })
+
+const { logger } = require('./lib/mlog')
+process.on('SIGINT', () => {
+  process.kill(process.pid)
+})
+logger.info(`PID: ${process.pid}`)
