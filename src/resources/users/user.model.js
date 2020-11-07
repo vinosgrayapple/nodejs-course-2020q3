f60c1faec65298502adf7398783e10e06764ff3f
Serhii Komarychev <komarichev@gmail.com>
2020-11-07 17:45:50 +0200

    fix hook model User findOneAndUpdate

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -29,13 +29,19 @@ UserSchema.pre('save', function(next) {
     })
   })
 })
-
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    if (err) return cb(err)
-    cb(null, isMatch)
+UserSchema.pre('findOneAndUpdate', function(next) {
+  const user = this._update
+  if (!user.password) return next()
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) return next(err)
+    bcrypt.hash(user.password, salt, (error, hash) => {
+      console.log(user.password, salt)
+      if (error) return next(error)
+      user.password = hash
+      next()
+    })
   })
-}
+})
 
 UserSchema.statics.toResponse = user => {
   const { id, name, login } = user
